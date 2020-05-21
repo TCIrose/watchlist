65021aa48721a4e9e14e07225168b624b9d404e6
Tony <57923336+TCIrose@users.noreply.github.com>
2020-05-22 00:36:45 +0300

    Making the API call and a few error corrections

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_movies
 
 #views
 
@@ -8,8 +9,12 @@ def index():
     '''
     view root page that returns the index page and its data
     '''
+
+    #Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
     title = "Home - welcome to the Best Movie Review Website Online"
-    return render_template('index.html', title = title, )#first message = in html doc, second html = views file
+    return render_template('index.html', title = title, popular = popular_movies )#first message = in html doc, second html = views file
 
 #dynamic routes
 @app.route('/movie/<int:movie_id>')
@@ -18,3 +23,4 @@ def movie(movie_id):
     View movie page function that returns the movie details page and its data
     '''
     return render_template('movie.html', id = movie_id)
+
