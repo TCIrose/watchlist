00952e10e7d8d461549a31ce8fcb4bc9deda2a18
Tony <57923336+TCIrose@users.noreply.github.com>
2020-05-25 08:07:07 +0300

    Unsaved changes saved

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
 from .config import DevConfig
-<<<<<<< HEAD
 from flask_bootstrap import Bootstrap
-=======
-from app import views
-from app import error
->>>>>>> c8a8b58661071f089157b914c0b99409fa0aa8b9
 
 #Initializing application
 app = Flask(__name__, instance_relative_config = True)
